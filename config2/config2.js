8352f6123ac625bf918676f448cbb7ff5b77b517
chann9 <foerzafaren@gmail.com>
2025-08-03 08:14:37 +0700

    sql

---
+++ config2/config2.js
@@ -0,0 +1,13 @@
+const DB_HOST = `sql.freedb.tech`;
+const DB_USER = `freedb_people`;
+const DB_PASSWORD = `ABzBqfRN$3c5ZEj`;
+const DB_NAME = `freedb_todo_saung_`;
+const DB_PORT = 3306;
+
+module.exports = {
+    DB_HOST,
+    DB_USER,
+    DB_PASSWORD,
+    DB_NAME,
+    DB_PORT,
+};
