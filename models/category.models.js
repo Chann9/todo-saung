cf4fa3243dc65dcaabacbce12fdcd1d83578a458
chann9 <foerzafaren@gmail.com>
2025-08-28 18:46:10 +0700

    feat: add CRUD operations for categories and todos, update HTTP request files

--- models/category.models.js
+++ models/category.models.js
@@ -1,16 +1,64 @@
 const { connectToDb } = require(".");
 
 class CategoryModel {
-    async getCategories() {
-        const connection = await connectToDb();
-        const [categories] = await connection.query(`SELECT * FROM kategori`);
-        await connection.end();
-        return categories;
-    }
-    //new
-    async getCategoriesById(id) {
-        const connection = await connectToDb();
-        const [categories] = await connection.query(`SELECT * FROM kategori WHERE id = ?`, [id]);
+  async getCategories() {
+    const connection = await connectToDb();
+    const [categories] = await connection.query(`SELECT * FROM kategori`);
+    await connection.end();
+    return categories;
+  }
+  //new
+  async getCategoryById(id) {
+    const connection = await connectToDb();
+    const [categories] = await connection.query(
+      `SELECT * FROM kategori WHERE id = ?`,
+      [id]
+    );
+    await connection.end();
+    return categories;
+  }
+
+  async createCategory(data) {
+    const { kategori_name } = data;
+    const connection = await connectToDb();
+    const [result] = await connection.query(
+      `INSERT INTO ategories (kategori_name) VALUES (?)`,
+      [kategori_name]
+    );
+    await connection.end();
+    return result;
+  }
+
+  async updateCategory(id, data) {
+    const connection = await connectToDb();
+    if (data.id) delete data.id;
+
+    let updateString = "";
+    const values = [];
+    Object.keys(data).forEach((key) => {
+      updateString += `${key} = ?,`;
+      values.push(data[key]);
+    });
+    updateString = updateString.slice(0, -1);
+    values.push(id);
+
+    const [result] = await connection.query(
+      `UPDATE kategori SET ${updateString} WHERE id = ?`,
+      values
+    );
+    await connection.end();
+    return result;
+  }
+
+  async deleteCategory(id) {
+    const connection = await connectToDb();
+    const [result] = await connection.query(
+      `DELETE FROM kategori WHERE id = ?`,
+      [id]
+    );
+    await connection.end();
+    return result;
+  }
 }
-}
+
 module.exports = new CategoryModel();
--- routers/todos.js
+++ routers/todos.js
@@ -1,11 +1,31 @@
-const express = require('express');
-const todoModels = require('../models/todos.models');
+const express = require("express");
+const todoModels = require("../models/todos.models");
 
 const router = express.Router();
 
-router.get('/', async function (req,res) {
-    const todos = await todoModels.getTodos(req.params.id);
-    return res.json(todos[0]);
+router.get("/", async function (req, res) {
+  const todos = await todoModels.getTodos(req.params.id);
+  return res.json(todos[0]);
+});
+
+router.get("/:id", async (req, res) => {
+  const todo = await todoModels.getTodoById(req.params.id);
+  res.json(todo[0]);
+});
+
+router.post("/", async (req, res) => {
+  const createTodo = await todoModels.createTodo(req.body);
+  res.json(createTodo);
+});
+
+router.put("/:id", async (req, res) => {
+  const updateTodo = await todoModels.updateTodo(req.params.id, req.body);
+  res.json({ success: updateTodo.affectedRows > 0 });
+});
+
+router.delete("/:id", async (req, res) => {
+  const deleteTodo = await todoModels.deleteTodo(req.params.id);
+  res.json({ success: deleteTodo.affectedRows > 0 });
 });
 
 module.exports = router;
