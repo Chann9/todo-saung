9f1f73d585f206f33fc639311913a651f1fcb1ac
chann9 <foerzafaren@gmail.com>
2025-08-29 11:59:03 +0700

    fix: update category routes to correct IDs in HTTP requests

--- routers/categories.js
+++ routers/categories.js
@@ -1,30 +1,30 @@
-const express = require('express');
-const categoryModels = require('../models/category.models');
+const express = require("express");
+const categoryModels = require("../models/category.models");
 
 const router = express.Router();
 
 //new category
-router.get('/', async function (req,res) {
-    const categories = await categoryModels.getCategories();
-    return res.json(categories);
+router.get("/", async function (req, res) {
+  const categories = await categoryModels.getCategories();
+  return res.json(categories);
 });
 
-router.get('/:id', async (req,res) => {
+router.get("/:id", async (req, res) => {
   const category = await categoryModels.getCategoryById(req.params.id);
   return res.json(category[0]);
 });
 
-router.post('/', async function (req,res) {
+router.post("/", async function (req, res) {
   const createCategory = await categoryModels.createCategory(req.body);
   res.json(createCategory);
 });
 
-router.put('/:id', async (req,res) => {
-  const updateCategory = await categoryModels.updateCategory(req.params.id, req.body);
+router.put("/:id", async (req, res) => {
+  const updateCategory = await categoryModels.updateCategory(req.params.id,req.body);
   res.json({ success: updateCategory.affectedRows > 0 });
 });
 
-router.delete('/:id', async (req,res) => {
+router.delete("/:id", async (req, res) => {
   const deleteCategory = await categoryModels.deleteCategory(req.params.id);
   res.json({ success: deleteCategory.affectedRows > 0 });
 });
