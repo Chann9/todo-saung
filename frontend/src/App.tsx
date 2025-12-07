95365ae7deaf80fb2346dfd4a4cd3271ea7976b0
chann9 <foerzafaren@gmail.com>
2025-12-07 14:18:01 +0700

    feat: add login and dashboard pages, update package.json for concurrent dev scripts
    - Implemented a new Dashboard page.
    - Created a Login page with form handling and API integration for authentication.
    - Updated package.json to include a new script for running both server and client concurrently.
    - Added 'concurrently' as a dependency in package.json and package-lock.json.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import LoginPage from './pages/Login';
+import DashboardPages from './pages/Dashboard';
+import {useState} from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+    const [currentPage] = useState(() => window.location.pathname);
 
-  return (
-    <>
+    if (currentPage === '/app/dashboard') {
+        return <DashboardPages />;
+    }
+
+    if (currentPage === '/app/login' || currentPage === '/app') {
+        return <LoginPage />;
+    }
+
+    return (
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>halaman tidak ditemukan</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    )
 }
-
-export default App
