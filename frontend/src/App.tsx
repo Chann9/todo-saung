e46212443648ad077994f4813e79fb0d998a8e4b
chann9 <foerzafaren@gmail.com>
2025-12-28 09:23:28 +0700

    feat: implement routing with react-router-dom, add Navbar and Chatbot page, enhance Dashboard with todos management

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,21 @@
+import {Routes, Route} from 'react-router-dom';
 import LoginPage from './pages/Login';
 import DashboardPages from './pages/Dashboard';
-import {useState} from 'react';
+import ChatbotPage from './pages/Chatbot';
+
+//1. app login
+//2. app dashboard
+//3. app/chatbot (new)
+//4. app/user-statistic (new)
 
 export default function App() {
-    const [currentPage] = useState(() => window.location.pathname);
-
-    if (currentPage === '/app/dashboard') {
-        return <DashboardPages />;
-    }
-
-    if (currentPage === '/app/login' || currentPage === '/app') {
-        return <LoginPage />;
-    }
-
-    return (
-      <div>
-        <h1>halaman tidak ditemukan</h1>
-      </div>
-    )
+  return (
+    <Routes>
+      {/*bisa mendefinisikan routing yg diperlukan*/}
+      <Route path= 'app/login' element= {<LoginPage/>} />
+      <Route path= 'app/dashboard' element= {<DashboardPages/>} />
+      <Route path= 'app/chatbot' element= {<ChatbotPage/>} />
+      <Route path= '*' element= {<p>Page not found</p>} />
+      </Routes>
+  )
 }
